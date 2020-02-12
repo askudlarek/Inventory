51d21838762956d100d43c97e260164b64acfa6c
Adam Skudlarek <skudthe20@gmail.com>
2020-02-12 11:16:43 -0800

    Adding the comments for Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -4,19 +4,20 @@
 
 using std::string;
 using std::ostream;
-  
+
+/* The constructor for inventory */
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
   m_price = price;
   m_in_stock = count;
 }
-
+/* Subtracting stock when an item is sold */
 void Inventory::sell()
 {
   m_in_stock--;
 }
-
+/* Printing out the inventory item */
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
   stream << item.m_name 
